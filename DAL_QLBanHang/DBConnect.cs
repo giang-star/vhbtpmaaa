f669bc134e1767fb6a902eab12caf0db86e77e65
giang-star <nvgianga9@gmail.com>
2022-03-24 12:45:29 +0700

    lan dau

---
+++ DAL_QLBanHang/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL_QLBanHang
+{
+   public class DBConnect
+    {
+        
+        protected SqlConnection _conn = new SqlConnection(@"Data Source=DESKTOP-81VHF3A\SQLEXPRESS;Initial Catalog=qlbh;Integrated Security=True");
+    }
+}
